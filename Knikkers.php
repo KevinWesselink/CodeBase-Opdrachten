c6d4ec80f9f0e400c54c52692e11d26e2bea8052
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-11-02 16:54:09 +0100

    If-else toegevoegd en wat comments.

--- Knikkers.php
+++ Knikkers.php
@@ -1,5 +1,12 @@
 <?php
 
+$getal = readline("Geef een getal. (Maximaal 20)");
 
+//For loopje nodig, om te blijven loopen, indien getal niet binnen 1 en 20 valt.
+if ($getal >= 1 && $getal <= 20) {
+    //Code
+} else {
+    $getal = readline("Probeer het opnieuw.");
+}
 
 ?>
