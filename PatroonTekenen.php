d1d0f18f23a8b7397fca8536d7315291ddab7eff
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2022-11-03 12:35:54 +0100

    If-else verwijderd, for toegevoegd.

--- PatroonTekenen.php
+++ PatroonTekenen.php
@@ -1,36 +1,13 @@
 <?php
 
-$ster = "*";
+$ster = "* ";
 $teller = 1;
 
 while ($teller < 6) {
-    if ($teller == 1) {
-        echo $ster . " \n";
-        $teller++;
-    } else if ($teller == 2) {
-        echo $ster . " ";
-        echo $ster . " \n";
-        $teller++;
-    } else if ($teller == 3) {
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " \n";
-        $teller++;
-    } else if ($teller == 4) {
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " \n";
-        $teller++;
-    } else if ($teller == 5) {
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " ";
-        echo $ster . " \n";
-        $teller++;
-        break;
+    for ($i = 0; $i != 5; $i++) {
+        echo $ster;
     }
+    $teller++;
 }
 
 ?>
