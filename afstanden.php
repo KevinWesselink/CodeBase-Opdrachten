b5dc5f420f3cfe3f97b18a2fd10484f2f1b7099d
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-09 12:32:48 +0100

    IndexSearch en $verschil toegevoegd.

--- afstanden.php
+++ afstanden.php
@@ -13,6 +13,10 @@ $gewensteGrootheid = readline("Geef een gewenste grootheid: ");
 if (is_numeric($waardeVoor) && $grootheidVoor != $gewensteGrootheid) {
     if ($grootheidVoor == $grootheden[0] || $grootheidVoor == $grootheden[1] || $grootheidVoor == $grootheden[2] || $grootheidVoor == $grootheden[3] || $grootheidVoor == $grootheden[4] || $grootheidVoor == $grootheden[5] || $grootheidVoor == $grootheden[6] && $gewensteGrootheid == $grootheden[0] || $gewensteGrootheid == $grootheden[1] || $gewensteGrootheid == $grootheden[2] || $gewensteGrootheid == $grootheden[3] || $gewensteGrootheid == $grootheden[4] || $gewensteGrootheid == $grootheden[5] || $gewensteGrootheid == $grootheden[6]) {
         //berekening omzetten afstanden
+        $indexVoor = array_search($grootheidVoor, $grootheden);
+        $indexNa = array_search($gewensteGrootheid, $grootheden);
+        $verschil = $indexVoor - $indexNa;
+        echo $verschil;
         //$waardeNa =
 
         echo "\n";
