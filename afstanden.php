8616b84d76c4a5cac48a72f6a6d8dd605168a364
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-09 12:18:42 +0100

    If-statement aangepast, echo's ingevoerd.

--- afstanden.php
+++ afstanden.php
@@ -10,17 +10,19 @@ $waardeNa = "";
 $grootheidVoor = readline("Geef een grootheid: ");
 $gewensteGrootheid = readline("Geef een gewenste grootheid: ");
 
-if (is_numeric($waardeVoor && $grootheidVoor != $gewensteGrootheid)) {
-    if ($grootheidVoor == $grootheden[0] || $grootheidVoor == $grootheden[1] || $grootheidVoor == $grootheden[2] || $grootheidVoor == $grootheden[3] || $grootheidVoor == $grootheden[4] || $grootheidVoor == $grootheden[5] || $grootheidVoor == $grootheden[6]) {
+if (is_numeric($waardeVoor) && $grootheidVoor != $gewensteGrootheid) {
+    if ($grootheidVoor == $grootheden[0] || $grootheidVoor == $grootheden[1] || $grootheidVoor == $grootheden[2] || $grootheidVoor == $grootheden[3] || $grootheidVoor == $grootheden[4] || $grootheidVoor == $grootheden[5] || $grootheidVoor == $grootheden[6] && $gewensteGrootheid == $grootheden[0] || $gewensteGrootheid == $grootheden[1] || $gewensteGrootheid == $grootheden[2] || $gewensteGrootheid == $grootheden[3] || $gewensteGrootheid == $grootheden[4] || $gewensteGrootheid == $grootheden[5] || $gewensteGrootheid == $grootheden[6]) {
         //berekening omzetten afstanden
         //$waardeNa =
 
-        echo "Welke waarde: " . $waardeVoor;
-        echo "Welke grootheid" . $grootheidVoor;
-        echo "Welke gewenste grootheid: " . $gewensteGrootheid;
-        echo $waardeVoor . " " . $grootheidVoor . " = " . $waardeNa . " " . $gewensteGrootheid;
+        echo "\n";
+        echo "Welke waarde: " . $waardeVoor . "\n";
+        echo "Welke grootheid" . $grootheidVoor . "\n";
+        echo "Welke gewenste grootheid: " . $gewensteGrootheid . "\n";
+        echo $waardeVoor . " " . $grootheidVoor . " = " . $waardeNa . " " . $gewensteGrootheid . "\n";
     } else {
-        echo "De grootheid die je hebt ingevuld is niet geldig.";
+        echo "Eén of meer grootheden die je hebt ingevuld zijn niet geldig.\n";
+        echo "Geldige grootheden zijn: kilometer, hectometer, decameter, meter, decimeter, centimeter, millimeter.\n";
     }
 } else {
     echo "Voer een geldige waarde in.\n";
