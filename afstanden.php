6e666222b4ebe759be576a92120f07a079739c50
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-10 12:41:51 +0100

    If-statement aangepast en daarbij een bug gefixt.

--- afstanden.php
+++ afstanden.php
@@ -11,7 +11,7 @@ $grootheidVoor = readline("Geef een grootheid: ");
 $gewensteGrootheid = readline("Geef een gewenste grootheid: ");
 
 if (is_numeric($waardeVoor) && $grootheidVoor != $gewensteGrootheid) {
-    if ($grootheidVoor == $grootheden[0] || $grootheidVoor == $grootheden[1] || $grootheidVoor == $grootheden[2] || $grootheidVoor == $grootheden[3] || $grootheidVoor == $grootheden[4] || $grootheidVoor == $grootheden[5] || $grootheidVoor == $grootheden[6] && $gewensteGrootheid == $grootheden[0] || $gewensteGrootheid == $grootheden[1] || $gewensteGrootheid == $grootheden[2] || $gewensteGrootheid == $grootheden[3] || $gewensteGrootheid == $grootheden[4] || $gewensteGrootheid == $grootheden[5] || $gewensteGrootheid == $grootheden[6]) {
+    if ((in_array($grootheidVoor, $grootheden) && in_array($gewensteGrootheid, $grootheden))) {
         //berekening omzetten afstanden
         $indexVoor = array_search($grootheidVoor, $grootheden);
         $indexNa = array_search($gewensteGrootheid, $grootheden);
