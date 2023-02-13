2625fde3901711c486bd73d1c90932e0e1b7b131
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-13 12:55:27 +0100

    Bestand én variabelen aangemaakt.

---
+++ BepaalMogelijkeOpties.php
@@ -0,0 +1,13 @@
+<?php
+
+$kostenWorst = 5;
+$verpakteWorsten = 6;
+$kostenHamburger = 10;
+$verpakteHamburgers = 20;
+$kostenFrikandellen = 15;
+$verpakteFrikandellen = 25;
+$budget = 100;
+
+
+
+?>
