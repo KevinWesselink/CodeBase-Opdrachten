77a5ae4df502c4cfecf945eab7b1d03c9921f257
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-20 12:12:58 +0100

    Nieuwe variabelen toegevoegd

--- BepaalMogelijkeOpties.php
+++ BepaalMogelijkeOpties.php
@@ -2,12 +2,13 @@
 
 $kostenWorst = 5;
 $verpakteWorsten = 6;
+$aantalWorsten = 0;
 $kostenHamburger = 10;
 $verpakteHamburgers = 20;
+$aantalHamburgers = 0;
 $kostenFrikandellen = 15;
 $verpakteFrikandellen = 25;
+$aantalFrikandellen = 0;
 $budget = 100;
-
-
 
 ?>
