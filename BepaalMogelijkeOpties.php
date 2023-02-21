d1b522ca238652226655bdf900179926c49f9af5
KevinWesselink <79516416+KevinWesselink@users.noreply.github.com>
2023-02-21 08:59:05 +0100

    Een aantal for-loops toegevoegd.

--- BepaalMogelijkeOpties.php
+++ BepaalMogelijkeOpties.php
@@ -11,4 +11,18 @@ $verpakteFrikandellen = 25;
 $aantalFrikandellen = 0;
 $budget = 100;
 
+//Worsten: (budget - kostenHamburger - kostenFrikandellen) / kostenWorst = 8;
+//Hamburgers: (budget - kostenWorst - kostenFrikandellen) / kostenHamburger = 8;
+//Frikandellen: (budget - kostenWorst - kostenHamburger) / kostenFrikandellen = 6;
+
+for ($w = 0; $w <= 8; $w++) {
+    //echo
+    for ($h = 0; $h <= 8; $h++) {
+        //echo
+        for ($f = 0; $f <= 6; $f++) {
+            //echo
+        }
+    }
+}
+
 ?>
